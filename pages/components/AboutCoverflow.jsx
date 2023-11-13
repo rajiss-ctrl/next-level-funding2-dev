04e930e6f84604e500755b9f14baf82360551cfe
Artiom Sincariov <asincariov@gmail.com>
2023-11-13 16:51:42 +0300

    Revert "Imported Image component to AboutCoverflow"
    This reverts commit 0824459e422f3041c99db162e96d45396a8a4ce0.

--- pages/components/AboutCoverflow.jsx
+++ pages/components/AboutCoverflow.jsx
@@ -6,8 +6,6 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
-import Image from "next/image";
-
 const Coverflow = () => {
   return (
     <div>
