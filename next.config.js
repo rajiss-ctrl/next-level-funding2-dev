e67d8106321d4625d5180c2898249a405a7434b3
rajiss-ctrl <omosanjos77#gmail.com>
2023-11-20 20:37:38 +0100

    loadash in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  modularizeImports: {
+    lodash: {
+      transform: 'lodash/{{member}}'
+    },
+    '@material-ui/core/': {
+      transform: '@material-ui/core/{{member}}'
+    },
+    '@material-ui/lab/': {
+      transform: '@material-ui/lab/{{member}}'
+    },
+    '@material-ui/icons/?(((\\w*)?/?)*)': {
+      transform: '@material-ui/icons/{{ matches.[1] }}/{{member}}'
+    }
+  },
 }
 
+
+
 module.exports = nextConfig
+
+
